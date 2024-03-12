f8c3889d33851ba6d11c92f140de6935470e454f
Shafi <Shafi.adam@tn.gov>
2024-03-12 14:57:35 -0700

    test

---
+++ addLob.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+get_input() {
+    local input_name="$1"
+    local input_value="${!input_name}"
+    echo "$input_value"
+}
+
+# Retrieve inputs from GitHub workflow
+app_name=$(get_input "app_name")
+environments=$(get_input "environments")
+regions=$(get_input "regions")
+subscription_ids=$(get_input "subscription_ids")
+
+# Construct the new app entry
+new_app_entry="  $app_name = {
+    environments = [\"${environments//,/\", \"\"}\"]
+    regions     = [\"${regions//,/\", \"\"}\"]
+    subscription_id = {
+$(echo "$subscription_ids" | sed 's/\(.*\)/\1 = "\1"/')
+    }
+}"
+
+echo $new_app_entry
+
+# Define the path to the Terraform file
+# tf_file="path/to/locals.tf"
+
+# Search for the line number where the lob block starts
+# lob_start_line=$(grep -n 'lob = {' "$tf_file" | cut -d ":" -f 1)
+
+# Search for the line number where the lob block ends
+# lob_end_line=$(grep -n '}' "$tf_file" | grep -A1 "^$lob_start_line" | tail -n1 | cut -d ":" -f 1)
+
+# Add the new app entry inside the lob block
+# sed -i "${lob_end_line}i$new_app_entry" "$tf_file"
